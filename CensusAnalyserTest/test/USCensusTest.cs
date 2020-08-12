4c51b34e912ca6665b6d2553e73eb780d17a5596
kn52 <ashish52922@gmail.com>
2020-08-12 10:36:29 +0530

    [Aashish]Added:Test for wrong US file path

--- CensusAnalyserTest/test/USCensusTest.cs
+++ CensusAnalyserTest/test/USCensusTest.cs
@@ -1,4 +1,5 @@
 ﻿using CensusAnalyser;
+using CensusAnalyser.exception;
 using NUnit.Framework;
 
 namespace CensusAnalyserTest.test
@@ -7,6 +8,7 @@ namespace CensusAnalyserTest.test
     {
         static readonly string testPath = "D:\\AAA\\VisualStudio\\CensusAnalyserSln\\CensusAnalyserTest";
         private readonly string US_CENSUS_FILE_PATH = testPath + "\\csvfiles\\USCensusData.csv";
+        private readonly string US_CENSUS_FILE_WRONG_PATH = testPath + "USCensusData.csv";
 
         CensusDataAnalyser censusDataAnalyser;
 
@@ -21,6 +23,14 @@ namespace CensusAnalyserTest.test
         {
             int count = censusDataAnalyser.ReadUSFile(US_CENSUS_FILE_PATH);
             Assert.AreEqual(51, count);
+        }
+
+        [Test]
+        public void GivenCSVFilePath_WhenIncorrect_willthrowException()
+        {
+            var ex = Assert.Throws<CensusDataAnalyserException>(
+                () => censusDataAnalyser.ReadUSFile(US_CENSUS_FILE_WRONG_PATH));
+            Assert.AreEqual("File Not Found", ex.Message);
         }
     }
 }
